850996cff0a99146a5b13701e6c21b365be0a885
stephtim <38559257+stephtim@users.noreply.github.com>
2025-09-15 11:06:15 +0300

    Create 1-with_db_connection.py

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,24 @@
+# decorator that opens a database connection, passes it to the function and closes it afterword
+import sqlite3
+import functools
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')  # Path to your database
+        try:
+            result = func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+        return result
+    return wrapper
+
+@with_db_connection 
+def get_user_by_id(conn, user_id): 
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
+    return cursor.fetchone() 
+
+# --- Fetch user by ID with automatic connection handling ---
+user = get_user_by_id(user_id=1)
+print(user)
