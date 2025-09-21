0d33d9381770e63fec933f0b9c36909b2728f1d1
Timothy <madindoustephen8@gmail.com>
2025-09-21 19:54:00 +0300

    "Mock Http Calls"

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -72,15 +72,6 @@ class TestAccessNestedMap(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
 
-#!/usr/bin/env python3
-"""Unit tests for utils.py module.
-"""
-
-
-#!/usr/bin/env python3
-"""Unit tests for utils.py module.
-"""
-
 
 
 
